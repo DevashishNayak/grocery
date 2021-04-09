c728ce662d1a7511b21d4708c124a3da6ec3a45c
Rahul Katre <smart1boy2rahul3@gmail.com>
2021-04-09 14:58:25 +0530

    Initial commit

---
+++ models/product.js
@@ -0,0 +1,32 @@
+const mongoose=require('mongoose');
+const ProductSchema=new mongoose.Schema({
+    name:{type:String,required:true},
+    product_image_2:{type:String,required:false},
+    product_image_1:{type:String,required:false},
+    brand:{type:String,required:false},
+    category:{type:String,required:true},
+    description:{type:String,required:true},
+    numReviews:{type:Number,default:0},
+    quantity:{type:Number,required:true,default:0},
+    price:{type:Number,required:true,default:0},
+    unit:{type:String,required:false}
+
+   
+   
+   }); 
+const CategorySchema=new mongoose.Schema({
+    name:{type:String,required:true},
+    description:{type:String,required:true},
+    image:{type:String,required:false}
+})
+
+const Banner_s=new mongoose.Schema({
+    title:{type:String},
+    description:{type:String},
+    redirect:{type:String},
+    image:{type:String}
+})
+const Banner=mongoose.model("Banner",Banner_s);
+const Category=mongoose.model("category",CategorySchema);
+const Product=mongoose.model('product',ProductSchema);
+module.exports={Product,Category,Banner};
---
+++ routes/productRoute.js
@@ -0,0 +1,225 @@
+
+const express = require('express');
+const Product = require('../models/product').Product;
+const Banner = require('../models/product').Banner;
+const Category = require('../models/product').Category;
+const Admin = require('../models/Admin');
+const formidable = require('formidable');
+const fs = require('fs');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+
+
+function get_admin(req) {
+  return true;
+}
+
+// *******************************FOR products
+router.get("/products", async (req, res) => {
+  const token = req.cookies.jwt;
+  jwt.verify(token, 'rahulk', async (err, decodedToken) => {
+    let admin = await Admin.findById(decodedToken.id);
+
+
+    const products = await Product.find();
+    res.render('products', { title: "Products", products: products, admin: admin })
+
+  });
+});
+
+router.get("/api/products", async (req, res) => {
+  const products = await Product.find();
+  res.json({ title: "Products", products: products })
+});
+
+router.post("/products", async (req, res) => {
+
+
+  console.log(req.body);
+
+  const product = new Product({
+    name: req.body.product_name,
+    price: req.body.product_price,
+    category: req.body.product_category,
+    product_image_1: req.body.product_image_1,
+    product_image_2: req.body.product_image_2,
+    quantity: req.body.product_qt,
+    description: req.body.product_description,
+    brand: req.body.product_brand,
+    unit: req.body.unit
+
+
+  });
+  const newProduct = await product.save();
+  if (newProduct) {
+
+    res.send({ message: `new product created ${newProduct.name}` })
+  }
+  else {
+    res.send({ message: "Errrore in creating product" })
+
+  }
+
+});
+
+router.get('/add_product', async (req, res) => {
+
+  const token = req.cookies.jwt;
+  jwt.verify(token, 'rahulk', async (err, decodedToken) => {
+    let admin = await Admin.findById(decodedToken.id);
+    const categories = await Category.find({});
+    res.render('add_product', { title: "add product", admin: admin, categories: categories });
+  });
+
+});
+
+router.get('/product/delete/:id', async (req, res) => {
+  const productId = req.params.id;
+  const deletedProduct = await Product.findByIdAndDelete(productId);
+  if (deletedProduct) {
+    res.redirect("/products");
+    console.log("product delted with id", deletedProduct._id);
+  }
+});
+
+router.get('/product/update/:id', async (req, res) => {
+  const productId = req.params.id;
+  const updateProductPage = await Product.findById(productId);
+
+  const token = req.cookies.jwt;
+  jwt.verify(token, 'rahulk', async (err, decodedToken) => {
+    let admin = await Admin.findById(decodedToken.id);
+    const categories = await Category.find({});
+    res.render('edit_product', { title: "edit product", admin: admin, products: updateProductPage, categories: categories });
+  });
+
+
+});
+
+router.post('/product/update/:id', async (req, res) => {
+  const productId = req.params.id;
+  const updateProduct = await Product.findByIdAndUpdate(productId,
+    {
+      description: req.body.description,
+      name: req.body.name,
+      price: req.body.price,
+      category: req.body.category,
+      quantity: req.body.quantity,
+      product_image_2: req.body.product_image_2,
+      product_image_1: req.body.product_image_1,
+      brand: req.body.brand,
+      unit: req.body.unit
+    });
+  if (updateProduct) {
+    res.redirect("/products");
+    console.log("product updated with id", updateProduct._id);
+  }
+});
+// *******************************FOR categories
+router.post("/categories", async (req, res) => {
+
+
+  console.log(req.body);
+
+  const category = new Category({
+    name: req.body.category_name,
+
+    description: req.body.category_description,
+    image: req.body.image
+
+
+  });
+  const new_Cate = await category.save();
+  if (new_Cate) {
+
+    res.send({ msg: `new Category created ${new_Cate.name}` })
+  }
+  else {
+    res.send({ msg: "Errrore in creating category" })
+
+  }
+
+});
+router.get('/add_category', (req, res) => {
+  const token = req.cookies.jwt;
+  jwt.verify(token, 'rahulk', async (err, decodedToken) => {
+    let admin = await Admin.findById(decodedToken.id);
+
+    res.render('add_category', { admin: admin, title: "Dashboard" })
+  });
+});
+router.get('/category/delete/:id', async (req, res) => {
+  const categoryId = req.params.id;
+  const deletedcategory = await Category.findByIdAndDelete(categoryId);
+  if (deletedcategory) {
+    res.redirect("/categories");
+    console.log("category delted with id", deletedcategory._id);
+  }
+});
+
+
+
+router.get("/categories", async (req, res) => {
+  const token = req.cookies.jwt;
+  jwt.verify(token, 'rahulk', async (err, decodedToken) => {
+    let admin = await Admin.findById(decodedToken.id);
+
+    const categories = await Category.find();
+
+    res.render('categories', { title: "Categories", categories: categories, admin: admin })
+  });
+});
+
+router.get("/api/categories", async (req, res) => {
+  const categories = await Category.find();
+
+  res.json({ title: "Categories", categories: categories })
+});
+
+
+
+
+
+
+
+//***************** * FOR BANNERS/
+
+
+router.get("/add_banner", async (req, res) => {
+  const token = req.cookies.jwt;
+  jwt.verify(token, 'rahulk', async (err, decodedToken) => {
+    let admin = await Admin.findById(decodedToken.id);
+
+
+
+    res.render('add_banner', { title: "Create Banner", admin: admin })
+  });
+});
+
+
+router.get("/banner", async (req, res) => {
+  const token = req.cookies.jwt;
+  jwt.verify(token, 'rahulk', async (err, decodedToken) => {
+    let admin = await Admin.findById(decodedToken.id);
+
+    const banner = await Banner.find();
+
+    res.render('banner', { title: " Banners", banner: banner, admin: admin })
+  });
+});
+
+
+
+router.get('/banner/delete/:id', async (req, res) => {
+  const bannerId = req.params.id;
+  const deletedbanner = await Banner.findByIdAndDelete(bannerId);
+  if (deletedbanner) {
+    res.redirect("/banner");
+    console.log("banner delted with id", deletedbanner._id);
+  }
+});
+
+
+
+
+module.exports = router;
